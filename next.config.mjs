bc84ea922222b20176421c42705f08934e5ad3bc
jonnyrallus <jonnyrallus@gmail.com>
2025-08-27 02:33:09 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import path from 'path';
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { unoptimized: true } };
+const nextConfig = {
+  images: { unoptimized: true },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(process.cwd());
+    return config;
+  },
+};
 export default nextConfig;
