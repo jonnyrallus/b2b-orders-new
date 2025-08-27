206e87b8a3899ea60bcc696184e4a913854ce64a
jonnyrallus <jonnyrallus@gmail.com>
2025-08-27 03:01:58 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import path from 'path';
 const nextConfig = {
   images: { unoptimized: true },
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(process.cwd()); // κάνει το "@" να δείχνει στη ρίζα
+    config.resolve.alias['@'] = path.resolve(process.cwd());
     return config;
   },
 };
